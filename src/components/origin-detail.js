c10ab697d179a98856cacf1b7d686aee0ff8c807
ceres0503 <ceres0503@icloud.com>
2016-08-25 21:00:27 -0600

    Update OriginPage

--- src/components/origin-detail.js
+++ src/components/origin-detail.js
@@ -1,72 +1,55 @@
 import React from 'react';
-import CoffeeStore from "../stores/coffee-store";
-import * as CoffeeActions from "../actions/coffee-actions";
+import OriginStore from "../stores/origin-store";
+import * as OriginActions from "../actions/origin-actions";
 
-export default class CoffeeDetail extends React.Component {
+export default class OriginDetail extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      coffees: [],
-      subscribedCoffeeId: 0
+      origin: {}
     };
 
-    CoffeeActions.loadCoffee({coffeeId: this.props.coffeeId});
+    OriginActions.loadOriginDetail(this.props.originId);
     this._onChange = this._onChange.bind(this);
   }
 
   componentDidMount() {
-    CoffeeStore.addChangeListener(this._onChange);
+    OriginStore.addChangeListener(this._onChange);
     //this._getCoffeeDataIfNeeded(this.props);
   }
 
   componentWillUnmount() {
-    CoffeeStore.removeChangeListener(this._onChange);
+    OriginStore.removeChangeListener(this._onChange);
   }
 
-  // componentWillReceiveProps(nextProps) {
-  //   this._getCoffeeDataIfNeeded(nextProps);
-  // }
+  componentWillReceiveProps(nextProps) {
+    this._getCoffeeDataIfNeeded(this.props, nextProps);
+  }
 
   _onChange() {
-    const coffees = CoffeeStore.getAll();
-    this.setState({coffees: coffees});
+    const origin = OriginStore.getOriginDetail();
+    this.setState({origin: origin});
+  }
+
+  _getCoffeeDataIfNeeded(currentProps, nextProps) {
+    if(this.props.originId !== nextProps.originId) {
+      OriginActions.loadOriginDetail(nextProps.originId);
+    }
   }
 
   render() {
-    // const {coffees} = this.state;
-    // if (coffees.length) {
-    //   const coffee = coffees[0];
-    //
-    const {coffees} = this.state;
-    if (coffees) {
+    const {origin} = this.state;
+    if (origin) {
       return (
-        <div id="coffee-detail" className="container">
-          {coffees.map((coffee) => (
-            <div key={coffee.id} className="row col-sm-12">
-              <div className="container col-sm-3">
-                <img src={coffee.image[0]} className="img-responsive block-center"/>
-              </div>
-              <div id="coffee-detail-info" className="container col-sm-8 col-sm-offset-1 well">
-                <h2 className="text-left">{coffee.name}</h2>
-                <p className="text-left">
-                  <span className="label label-info">{coffee.origin.name}</span>
-                </p>
-                <p className="text-left">{coffee.description}</p>
-                <p>
-                  <a className="btn btn-primary" href="#" role="button" onClick={this._handleSubscribe.bind(this, coffee)}>Subscribe Me!</a>
-                </p>
-              </div>
-            </div>
-          ))}
+        <div id="origin-detail" className="container">
+          <div id="origin-detail-info" className="row col-sm-12 well">
+            <h2 className="text-left">{origin.name}</h2>
+            <p className="text-left">{origin.description}</p>
+          </div>
         </div>
       );
     } else {
       return <div>Loading...</div>;
     }
-  }
-
-  _handleSubscribe(coffee, event) {
-    event.preventDefault();
-    this.setState({subscribedCoffeeId: coffee.id});
   }
 }
