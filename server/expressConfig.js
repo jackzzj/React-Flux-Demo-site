e6fc1d6994bd14fe447cccfa1764af0b647534da
jojohappy <daijun@hupu.com>
2016-08-30 22:01:14 +0800

    Fixed public path

--- server/expressConfig.js
+++ server/expressConfig.js
@@ -6,13 +6,13 @@ module.exports = function(app, express) {
   // Serve static assets from the app folder. This enables things like javascript
   // and stylesheets to be loaded as expected. You would normally use something like
   // nginx for this, but this makes for a simpler demo app to just let express do it.
-  
-  //app.use("/", express.static("public/"));
+
+  app.use("/", express.static("public/"));
   //app.use('/vendor/bootstrap', express.static(__dirname + '/../node_modules/bootstrap/dist/'));
   //app.use('/vendor/jquery', express.static(__dirname + '/../node_modules/jquery/dist/'));
 
   // Set the view directory, this enables us to use the .render method inside routes
-  //app.set('views', __dirname + '/../app/views');
+  app.set('views', __dirname + '/../public');
 
   // parse application/x-www-form-urlencoded
   app.use(bodyParser.urlencoded({
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public/assets/build'),
     filename: 'bundle.js',
-    publicPath: 'public/assets/build/'
+    publicPath: '/assets/build/'
   },
   module: {
     loaders: [{
